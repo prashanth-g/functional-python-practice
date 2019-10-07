39d59d815a41997a52fc15be124ff6376efc39a0
prashanth-g <prashanthcsgce@gmail.com>
2019-10-07 14:47:22 +0530

    Updated code for lint suggestions in test_pipeline

--- tests/pipleline_test.py
+++ tests/pipleline_test.py
@@ -8,7 +8,7 @@ def test_pipeline():
          {'name': 'Pixed', 'country': 'US', 'active': True},
          {'name': 'Windows', 'country': 'India', 'active': False}]
     result = pipeline.pipeline(mobile_phones, [pipeline.call(lambda x: 'Japan', 'country')])
-    assert(list(result)[0].get('country') is 'Japan')
+    assert(list(result)[0].get('country') == 'Japan')
 
 def test_pipeline_without_country_field():
     mobile_phones = [{'name': 'Redmi', 'active': True},
